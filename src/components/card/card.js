22c22b639f0667d12968bbdab37518ed57dc36c5
YujieLyu <jessie.yujie.lyu@gmail.com>
2020-09-10 10:45:30 +1000

    add card list and card container to display monsters

---
+++ src/components/card/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+const Card = props => {
+    return (
+        <div className="card-container">
+            <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+            <h1>{props.monster.name}</h1>
+            <p>{props.monster.email}</p>
+        </div>
+    )
+}
+
+export default Card
